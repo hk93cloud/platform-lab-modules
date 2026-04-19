e2e23c22269be82d4e6b9af2e7feb24a4bbed52f
Haitham Alrubai <alrubai.haitham@gmail.com>
2026-04-19 09:40:01 -0500

    update module

--- modules/compute/app-service/main.tf
+++ modules/compute/app-service/main.tf
@@ -5,6 +5,10 @@ resource "azurerm_linux_web_app" "this" {
   service_plan_id           = var.service_plan_id
   virtual_network_subnet_id = var.vnet_subnet_id
 
+  # Disable basic auth publishing to avoid "listing Site Publishing Credential" errors
+  ftp_publish_basic_authentication_enabled       = false
+  webdeploy_publish_basic_authentication_enabled = false
+
   site_config {
     application_stack {
       java_version        = var.java_version
